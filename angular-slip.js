460190b3198915120f813f8b7c9868ea730e6aea
Ashley Davis <ashley@codecapers.com.au>
2015-01-22 20:28:36 +1000

    Moved the event handler into the directive.

--- angular-slip.js
+++ angular-slip.js
@@ -10,7 +10,34 @@
 
 			link: function (scope, element, attrs) {
 
-				new Slip(element[0]);
+				var el = element[0];
+
+				el.addEventListener('slip:beforereorder', function(e){
+				    if (/demo-no-reorder/.test(e.target.className)) {
+				        e.preventDefault();
+				    }
+				}, false);
+
+				el.addEventListener('slip:beforeswipe', function(e){
+				    if (e.target.nodeName == 'INPUT' || /demo-no-swipe/.test(e.target.className)) {
+				        e.preventDefault();
+				    }
+				}, false);
+
+				el.addEventListener('slip:beforewait', function(e){
+				    if (e.target.className.indexOf('instant') > -1) e.preventDefault();
+				}, false);
+
+				el.addEventListener('slip:afterswipe', function(e){
+				    e.target.parentNode.appendChild(e.target);
+				}, false);
+
+				el.addEventListener('slip:reorder', function(e){
+				    e.target.parentNode.insertBefore(e.target, e.detail.insertBefore);
+				    return false;
+				}, false);
+
+				new Slip(el);
 			},
 		};
 	});
