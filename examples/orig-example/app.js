16eb73d8a1ec992c49e1b6f206ea63ee96a03baf
Ashley Davis <ashley@codecapers.com.au>
2015-01-24 11:42:47 +1000

    Updated example to remove unnecessary returns

--- examples/orig-example/app.js
+++ examples/orig-example/app.js
@@ -13,27 +13,22 @@ angular.module('app', [ 'slip' ])
 	    if (/demo-no-reorder/.test(e.target.className)) {
 	        e.preventDefault();
 	    }
-	    return false;
 	};
 
 	$scope.beforeSwipe = function (e) {
 	    if (e.target.nodeName == 'INPUT' || /demo-no-swipe/.test(e.target.className)) {
 	        e.preventDefault();
 	    }
-	    return false;
 	};
 
 	$scope.beforeWait = function (e) {
 		if (e.target.className.indexOf('instant') > -1) {
 			e.preventDefault();
 		}
-
-		return false;
 	};
 
 	$scope.afterSwipe = function (e) {
 		e.target.parentNode.appendChild(e.target);
-		return false;
 	};
 
 	$scope.reorder = function (e) {
